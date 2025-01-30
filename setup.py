ad9e70a9ba1a74ccf8df97a3f7fcdc9137e68b21
Kia Bazargan <104933008+kiabuzz@users.noreply.github.com>
2025-01-30 10:04:48 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treelut',
-    version='1.0.0',
+    version='1.0.2',
     author='Alireza Khataei, Kia Bazargan',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
